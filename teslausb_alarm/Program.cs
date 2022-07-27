f473706f75a8493013dec9a5808301d0afbc66b4
perpet99 <perpet99@gmail.com>
2022-07-27 20:11:03 +0900

    init code

---
+++ teslausb_alarm/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace teslausb_alarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string watcherPath;
+            FileSystemWatcher watcher;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) == true)
+            {
+                // windows test code
+                watcherPath = @"media/SentryClips";
+            }
+            else
+            {
+                watcherPath = @"/mutable/TeslaCam/SentryClips";
+            }
+
+            watcher = new FileSystemWatcher(watcherPath);
+            watcher.Created += Watcher_Created;
+        }
+
+        static List<string> WatcherFileList = new List<string>();
+
+        private static void Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            //ArchiveCallCount = 0;
+
+            if (WatcherFileList.Contains(e.FullPath))
+                return;
+            WatcherFileList.Add(e.FullPath);
+
+            string message = $"event sentry file : {e.FullPath} ";
+
+            GetSnsNCall(message);
+
+        }
+
+
+        public static void GetSnsNCall(string message)
+        {
+            Console.WriteLine(message);
+
+            //var url = "https://discordapp.com/api/webhooks/885042624581484594/4GyiWg6HyFHWPyucWLLcF-MGmaKxT--sQaTWRrZ7csXK3tnelAc7IzcfSiaWcfkJzQvP";
+            // set your url
+            var url = GetSns();
+            
+            if (url != null && 0 < url.Length)
+                WebCall.Call(url, message);
+
+        }
+        public static string GetConfigPath(string filename)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) == true)
+            {
+                return filename;
+            }
+            return "/mutable/" + filename;
+        }
+
+
+        public static string GetSns()
+        {
+            try
+            {
+                return System.IO.File.ReadAllText(GetConfigPath("sns.bin"));
+            }
+            catch (Exception)
+            {
+
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ teslausb_alarm/WebCall.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+
+namespace teslausb_alarm
+{
+
+    class FileWacher
+    {
+        private PhysicalFileProvider _fileProvider;
+        private IChangeToken _fileChangeToken;
+        public Action _action;
+
+        public FileWacher(string path)
+        {
+            _fileProvider = new PhysicalFileProvider(path);
+            WatchForFileChanges();
+
+        }
+
+        private void WatchForFileChanges()
+        {
+            _fileChangeToken = _fileProvider.Watch("*.*");
+            _fileChangeToken.RegisterChangeCallback(Notify, default);
+        }
+
+        private void Notify(object state)
+        {
+            Console.WriteLine("File change detected");
+            _action();
+
+            WatchForFileChanges();
+        }
+
+    }
+
+
+    class WebCall
+    {
+
+        public static void Call(string url, string message)
+        {
+            CallWebAPIAsync(url, message).Wait();
+        }
+
+        public class Data
+        {
+            public string content;
+        }
+
+        static async Task CallWebAPIAsync(string url, string message)
+        {
+            try
+            {
+                var data = new Data() { content = message };
+
+                var json = JsonConvert.SerializeObject(data);
+                var c = new StringContent(json, Encoding.UTF8, "application/json");
+
+                //var url = "https://discordapp.com/api/webhooks/885042624581484594/4GyiWg6HyFHWPyucWLLcF-MGmaKxT--sQaTWRrZ7csXK3tnelAc7IzcfSiaWcfkJzQvP";
+                using var client = new HttpClient();
+
+                var response = await client.PostAsync(url, c);
+
+                string result = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
