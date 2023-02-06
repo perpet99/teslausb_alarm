e5100f69e2820e59af11ae618385d1e1bed0cedf
DESKTOP-8GUEOOP\koo bon soo <perpet99@gmail.com>
2023-02-06 18:43:14 +0900

    init add code

--- teslausb_alarm/Program.cs
+++ teslausb_alarm/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace teslausb_alarm
 {
@@ -9,22 +10,52 @@ namespace teslausb_alarm
     {
         static void Main(string[] args)
         {
-
+            if (args.Length != 0)
+            {
+                if (SetSns(args[0]))
+                    Console.WriteLine($"sns addr save success : {args[0]}");
+                else
+                    Console.WriteLine($"sns addr save fail : {args[0]}");
+                return;
+            }
             string watcherPath;
             FileSystemWatcher watcher;
 
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) == true)
             {
                 // windows test code
-                watcherPath = @"media/SentryClips";
+                watcherPath = $"{System.IO.Directory.GetCurrentDirectory()}/SentryClips";
+
             }
             else
             {
                 watcherPath = @"/mutable/TeslaCam/SentryClips";
             }
+            
+            Console.WriteLine($"filesystem watcher : {watcherPath}");
+            if(System.IO.Directory.Exists(watcherPath) == false)
+                System.IO.Directory.CreateDirectory(watcherPath);
 
             watcher = new FileSystemWatcher(watcherPath);
             watcher.Created += Watcher_Created;
+            watcher.Filter = "*.*";
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("Press ctrl-c to exit.");
+
+            // Ctrl-c will gracefully exit the call at any point.
+            ManualResetEvent exitMre = new ManualResetEvent(false);
+            Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
+            {
+                e.Cancel = true;
+                exitMre.Set();
+            };
+
+            // Wait for a signal saying the call failed, was cancelled with ctrl-c or completed.
+            exitMre.WaitOne();
+
+
         }
 
         static List<string> WatcherFileList = new List<string>();
@@ -35,6 +66,7 @@ namespace teslausb_alarm
 
             if (WatcherFileList.Contains(e.FullPath))
                 return;
+
             WatcherFileList.Add(e.FullPath);
 
             string message = $"event sentry file : {e.FullPath} ";
@@ -77,6 +109,20 @@ namespace teslausb_alarm
 
             }
             return string.Empty;
+        }
+
+        public static bool SetSns(string addr)
+        {
+            try
+            {
+                System.IO.File.WriteAllText(GetConfigPath("sns.bin"),addr);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return false;
         }
     }
 }
